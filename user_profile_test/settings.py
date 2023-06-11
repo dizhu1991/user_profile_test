06e285ff0d5e5ddf6a942841cd1413c027fb900f
Di Zhu <di.zhu.jude@gmail.com>
2023-06-11 17:46:47 +1200

    retry secrets

--- user_profile_test/settings.py
+++ user_profile_test/settings.py
@@ -18,8 +18,8 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
-#TODO: figure out how to properly do env variables
-SECRET_KEY = 'this-is-not-it'
+# TODO: figure out how to properly do env variables
+SECRET_KEY = env('SECRET_KEY')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
