5b48b233b5f535b6868878388475af362687d4fc
Di Zhu <di.zhu.jude@gmail.com>
2023-06-11 16:33:30 +1200

    django-environ install

--- user_profile_test/settings.py
+++ user_profile_test/settings.py
@@ -13,8 +13,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from datetime import timedelta
 
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+
+SECRET_KEY = env('SECRET_KEY')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
