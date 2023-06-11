505735ae2b78236f9c68783f084139f2a9389770
Di Zhu <di.zhu.jude@gmail.com>
2023-06-11 17:37:39 +1200

    test with a random string

--- user_profile_test/settings.py
+++ user_profile_test/settings.py
@@ -19,7 +19,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'this-is-not-it'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
