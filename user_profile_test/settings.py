a95bde2364ecf2f9b399389070a58a0c83e9b473
Di Zhu <di.zhu.jude@gmail.com>
2023-06-11 17:26:07 +1200

    move tests folder; os env

--- user_profile_test/settings.py
+++ user_profile_test/settings.py
@@ -19,7 +19,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
